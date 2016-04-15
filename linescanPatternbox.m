b32c83cc1b6cfb06ab0176d59426661ce4dbb7f9
Thomas Lapauw <Thomas Lapauw>
2016-04-15 17:18:06 +0200

    changed to detect center of pattern box and to be able to start from offset.

--- linescanPatternbox.m
+++ linescanPatternbox.m
@@ -1,14 +1,16 @@
-function [ indexOfCenterbox ] = linescanPatternbox(line)
+function [ indexOfCenterbox ] = linescanPatternbox(line,startingIndex)
 %UNTITLED3 Summary of this function goes here
 %   Detailed explanation goes here
-    patternboxRatios = [1 1 3 1 1]
+    patternboxRatios = [1 1 3 1 1];
     detectedPixels = zeros(1,5);
     
-    centerboxIndex = 0;
-    x = 1; 
+    errorMargin = 0.5; %allowed deviation from ratio; 0.5 is standard
     
-    lineLength = length(line)
-    currentState = 'reset'
+    centerboxIndex = 0;
+    x = startingIndex; 
+    
+    lineLength = length(line);
+    currentState = 'reset';
     patternStart = 0;
     
 
@@ -19,7 +21,6 @@ function [ indexOfCenterbox ] = linescanPatternbox(line)
                 
                 if ~line(x) %if the current pixel is black, possibly the black bar is detected
                     detectedPixels = zeros(1,5); %reset the pixel counting vector
-                    display(x,'started new pattern from: ');
                     state = 'firstBlack'; %advance state to first black bar
                     detectedPixels(1) = 1; %count 1 pixel for the black bar
                 else
@@ -40,13 +41,13 @@ function [ indexOfCenterbox ] = linescanPatternbox(line)
                  else
                      state = 'centerBox';
                      detectedPixels(3) = 1;
-                     centerboxIndex = x
+                     centerboxIndex = x;
                      
                      
                      %check the ratio of the first black and white region
                      ratioFound = detectedPixels(1)/detectedPixels(2);
                      ratioRef =  patternboxRatios(1)/patternboxRatios(2);
-                     if ratioFound > 1.5 || ratioFound < 0.5 %check if the ratio between boxes is OK
+                     if ratioFound > (ratioRef + errorMargin) || ratioFound < (ratioRef - errorMargin) %check if the ratio between boxes is OK
                          state = 'reset';
                          x = x-1; % decrease x with one to compensate for the + 1 ath the end of the algorithm
                      end
@@ -63,7 +64,7 @@ function [ indexOfCenterbox ] = linescanPatternbox(line)
                      %check the ratio of the centerbox black and first white region
                      ratioFound = detectedPixels(3)/detectedPixels(2);
                      ratioRef =  patternboxRatios(3)/patternboxRatios(2);
-                     if ratioFound > 3.5 || ratioFound < 2.5 %check if the ratio between boxes is OK
+                     if ratioFound > (ratioRef + errorMargin) || ratioFound < (ratioRef - errorMargin) %check if the ratio between boxes is OK
                          state = 'reset';
                          x = centerboxIndex-1; % go back to the presumed centerbox to restart with pattern recongnition,-1 to compensate for the + 1 ath the end of the algorithm
                      end
@@ -80,7 +81,7 @@ function [ indexOfCenterbox ] = linescanPatternbox(line)
                      %check the ratio of the first black and white region
                      ratioFound = detectedPixels(3)/detectedPixels(4);
                      ratioRef =  patternboxRatios(3)/patternboxRatios(4);
-                     if ratioFound > 3.5 || ratioFound < 2.5 %check if the ratio between boxes is OK
+                     if ratioFound > (ratioRef + errorMargin) || ratioFound < (ratioRef - errorMargin) %check if the ratio between boxes is OK
                          state = 'reset';
                          x = centerboxIndex-1; % go back to the presumed centerbox to restart with pattern recongnition,-1 to compensate for the + 1 ath the end of the algorithm
                      end
@@ -97,13 +98,13 @@ function [ indexOfCenterbox ] = linescanPatternbox(line)
                                           %check the ratio of the first black and white region
                      ratioFound = detectedPixels(4)/detectedPixels(5);
                      ratioRef =  patternboxRatios(4)/patternboxRatios(5);
-                     if ratioFound > 1.5 || ratioFound < 0.5 %check if the ratio between boxes is OK
+                     if ratioFound > (ratioRef + errorMargin) || ratioFound < (ratioRef - errorMargin) %check if the ratio between boxes is OK
                          state = 'reset';
                          x = centerboxIndex -1; % go back to the presumed centerbox to restart with pattern recongnition,-1 to compensate for the + 1 ath the end of the algorithm
                      end
                 end
             case 'patternEnd'
-                indexOfCenterbox = centerboxIndex;
+                indexOfCenterbox = round(centerboxIndex + detectedPixels(3)/2);
                 return
         end
         x = x+1;
--- linescanTest.m
+++ linescanTest.m
@@ -2,16 +2,28 @@ clear
 close all
 
 
-Original = imread('test.jpg','jpg'); %read image
+% Original = imread('test.jpg','jpg'); %read image
+% 
+% Image = imresize(Original,[500,500]); % resize image
+% grey = rgb2gray(Image); %convert to grayscale
+grey = imread('QR.jpg','jpg'); %read image
 
-Image = imresize(Original,[500,500]); % resize image
-grey = rgb2gray(Image); %convert to grayscale
+
 mono = binarize(im2double(grey),0.5); % convert to double and binarize with a threshold of 0.5
 figure
 imshow(mono); % show mono image
 
 hold on
-for i = 1:500
-   plot(linescanPatternbox(mono(i,:)),i,'ro')
-   plot(i,linescanPatternbox(mono(:,i).'),'bo')
+
+for i = 1:330
+    horizontalCor = linescanPatternbox(mono(i,:),1);
+   %plot(horizontalCor,i,'ro');
+   if horizontalCor > 0
+       verticalCor = linescanPatternbox(mono(:,horizontalCor+1).',1);
+       if verticalCor > 0
+        plot(horizontalCor,verticalCor,'w*');
+       end
+   end
 end
+
+
