86dc67c06eb85d0ea37ce1c4bc9eb12464e61253
Thomas Lapauw <Thomas Lapauw>
2016-04-15 14:06:38 +0200

    able to convert image to monocolor image

--- QR_reader.m
+++ QR_reader.m
@@ -1,1 +1,18 @@
-a =1
+close all
+clear
+
+
+Original = imread('test.jpg','jpg'); %read image
+imshow(Original); % show original image
+title('Original image');
+
+Image = imresize(Original,[500,500]); % resize image
+grey = rgb2gray(Image); %convert to grayscale
+figure;
+imshow(grey); %show image in grayscale
+mono = binarize(im2double(grey),0.5); % convert to double and binarize with a threshold of 0.5
+figure
+imshow(mono); % show mono image
+
+
+
